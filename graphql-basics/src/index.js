d84dc43e522ee135bbdfb1dac410df9b058fd62e
Eyitayo Olaigbe <tayoolaigbe@outlook.com>
2021-08-16 11:42:51 +0100

    Set up basic Graphql server

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,1 +1,25 @@
-console.log('Hello Graphql');
+const { ApolloServer } = require('apollo-server');
+
+const typeDefs = `
+type Query {
+  hello: String!
+  name: String!
+  location: String!
+  bio: String!
+}
+`;
+const resolvers = {
+	Query: {
+		hello: () => 'This is my first query!',
+		name: () => 'Eyitayo Olaigbe',
+		bio: () => 'CEO and Founder of gidi',
+		location: () => 'Edinburgh, UK',
+	},
+};
+
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+});
+
+server.listen().then(({ url }) => console.log(`Server is running on ${url}`));
