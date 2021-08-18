f0488aec8a219124d9e6d58ded8bd21d2538976d
Eyitayo Olaigbe <tayoolaigbe@outlook.com>
2021-08-18 21:11:54 +0100

    Added createPost and createComment mutation

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -83,6 +83,8 @@ const typeDefs = `
 
 	type Mutation {
 		createUser(name: String!, email: String!, age: Int): User!
+		createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
+		createComment(text: String!, author: ID!, post: ID!): Comment!
 	}
 
 	type User {
@@ -166,6 +168,42 @@ const resolvers = {
 			users.push(user);
 
 			return user;
+		},
+		createPost: (parent, args, ctx, info) => {
+			const userExists = users.some(user => user.id === args.author);
+			if (!userExists) throw new Error('User not Found!');
+
+			const post = {
+				id: uuidv4(),
+				title: args.title,
+				body: args.body,
+				published: args.published,
+				author: args.author,
+			};
+
+			posts.push(post);
+
+			return post;
+		},
+		createComment: (parent, args, ctx, info) => {
+			const userExists = users.some(user => user.id === args.author);
+			const postExists = posts.some(
+				post => post.id === args.post && post.published
+			);
+
+			if (!userExists || !postExists)
+				throw new Error('User or Post does not exist!');
+
+			const comment = {
+				id: uuidv4(),
+				text: args.text,
+				author: args.author,
+				post: args.post,
+			};
+
+			comments.push(comment);
+
+			return comment;
 		},
 	},
 	Post: {
