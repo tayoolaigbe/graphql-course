bec5a03394192e8fb07bbfb87f97b1b6b34be5f1
Eyitayo Olaigbe <tayoolaigbe@outlook.com>
2021-08-25 15:34:55 +0100

    Addede referentialActions: onDelete

--- graphql-basics/src/script.js
+++ graphql-basics/src/script.js
@@ -12,43 +12,43 @@ async function main() {
 	// 		email: 'eyitayo@example.com',
 	// 	},
 	// });
-	// const userToDel = await prisma.user.delete({
-	// 	where: {
-	// 		id: 'cksjg5hny0000lkgfzvao25sq',
-	// 	},
-	// });
+	const userToDel = await prisma.user.delete({
+		where: {
+			id: 'cksrkr35v00004kgf5ylrj8v1',
+		},
+	});
 	// const postToDel = await prisma.post.delete({
 	// 	where: {
-	// 		id: 'cksjg7he90000kkgfzx174sc8',
+	// 		id: 'cksjh4inx000004gfrqtmg51m',
 	// 	},
 	// });
 	// const post = await prisma.post.create({
 	// 	data: {
-	// 		title: 'Second post',
-	// 		body: 'This is my second post',
+	// 		title: 'First post',
+	// 		body: 'This is my first post',
 	// 		published: true,
 	// 		author: {
 	// 			connect: {
-	// 				id: 'cksjh4inx000004gfrqtmg51m',
+	// 				id: 'cksrkr35v00004kgf5ylrj8v1',
 	// 			},
 	// 		},
 	// 	},
 	// });
-	const comment = await prisma.comment.create({
-		data: {
-			text: 'This post is actually a good read',
-			author: {
-				connect: {
-					id: 'ckspumyk90000dggfpynjm8lf',
-				},
-			},
-			post: {
-				connect: {
-					id: 'cksjh67wc00001sgfmv05mj5n',
-				},
-			},
-		},
-	});
+	// const comment = await prisma.comment.create({
+	// 	data: {
+	// 		text: 'This post is actually a good read',
+	// 		author: {
+	// 			connect: {
+	// 				id: 'cksrkr35v00004kgf5ylrj8v1',
+	// 			},
+	// 		},
+	// 		post: {
+	// 			connect: {
+	// 				id: 'cksrksp5w0000v4gfa89swl23',
+	// 			},
+	// 		},
+	// 	},
+	// });
 	const allUsers = await prisma.user.findMany();
 	console.log(allUsers);
 }
